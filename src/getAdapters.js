c559a5cc6a7beee667ef8ee510c7b631e1904c3e
Kyle <the.big.kb123@gmail.com>
2021-08-18 10:58:22 -0400

    Adding test adapters

--- src/getAdapters.js
+++ src/getAdapters.js
@@ -1,9 +1,13 @@
 const localAdapters = require('../config/adapters.local1.json')
+const testAdapters = require('../config/adapters.test1.json')
 const goerliAdapters = require('../config/adapters.goerli1.json')
 const mainnetAdapters = require('../config/adapters.mainnet1.json')
 
 const getAdapters = (env) => {
   switch(env) {
+    case 'test':
+      return testAdapters
+      break
     case 'local':
       return localAdapters
       break
